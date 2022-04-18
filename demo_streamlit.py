c37af8603591e096fb04bfe9fd5a19d3fb6400c5
Pribadi HCR <pribadihcr@users.noreply.github.com>
2022-04-18 12:04:30 +0800

    Add files via upload

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -1,7 +1,25 @@
 import streamlit as st
 from PIL import Image
- 
-st.title('Car Model Classification')
+import argparse
+import os
+import csv
+import glob
+import time
+import logging
+# from mxnet.ndarray.gen_op import LinearRegressionOutput
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+from collections import OrderedDict
+from contextlib import suppress
+
+from timm.models import create_model, apply_test_time_pool, load_checkpoint, is_model, list_models
+from timm.data import create_dataset, create_loader, resolve_data_config, RealLabelsImagenet
+from timm.utils import accuracy, AverageMeter, natural_key, setup_default_logging, set_jit_legacy
+
+import numpy as np
+
+st.title('Industrial Visualization')
 
 st.sidebar.header("User Input Image")
 
